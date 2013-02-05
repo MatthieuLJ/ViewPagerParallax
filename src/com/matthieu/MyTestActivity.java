5bfbc8c7a7187ac939ce0930cfd6624a4a5ac64f
Matthieu Jeanson <matthieu@touchrev.com>
2013-02-04 16:56:45 -0800

    Initial commit

---
+++ src/com/matthieu/MyTestActivity.java
@@ -0,0 +1,69 @@
+package com.matthieu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MyTestActivity extends Activity {
+    private static final int MAX_PAGES = 10;
+
+    private int num_pages = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        final ViewPagerParallax pager = (ViewPagerParallax) findViewById(R.id.pager);
+        pager.set_max_pages(MAX_PAGES);
+        pager.setBackgroundAsset(R.raw.sanfran);
+        pager.setAdapter(new my_adapter());
+
+        final Button add_page_button = (Button) findViewById(R.id.add_page_button);
+        add_page_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num_pages = Math.min(num_pages+1, MAX_PAGES);
+                pager.getAdapter().notifyDataSetChanged();
+            }
+        });
+    }
+
+    private class my_adapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return num_pages;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return view == o;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View new_view=null;
+
+            LayoutInflater inflater = getLayoutInflater();
+            new_view = inflater.inflate(R.layout.page, null);
+            TextView num = (TextView) new_view.findViewById(R.id.page_number);
+            num.setText(Integer.toString(position));
+
+            container.addView(new_view);
+
+            return new_view;
+        }
+
+    }
+}
+
---
+++ src/com/matthieu/ViewPagerParallax.java
@@ -0,0 +1,138 @@
+package com.matthieu;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.*;
+import android.os.Build;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ViewPagerParallax extends ViewPager {
+    int background_id =-1;
+    int background_saved_id =-1;
+    int saved_width=-1, saved_height=-1, saved_max_num_pages =-1;
+    Bitmap saved_bitmap;
+
+    int max_num_pages=0;
+    int imageHeight;
+    int imageWidth;
+    float zoom_level;
+
+    Rect r = new Rect();
+
+    private final static String TAG="ViewPagerParallax";
+
+    public ViewPagerParallax(Context context) {
+        super(context);
+    }
+
+    public ViewPagerParallax(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @SuppressLint({"NewApi"})
+    private void set_new_background() {
+        if (background_id == -1)
+            return;
+
+        if (max_num_pages == 0)
+            return;
+
+        if (getWidth()==0 || getHeight()==0)
+            return;
+
+        if ((saved_height == getHeight()) && (saved_width == getWidth()) &&
+                (background_saved_id==background_id) &&
+                (saved_max_num_pages == max_num_pages))
+            return;
+
+        InputStream is;
+
+        try {
+            is = getContext().getResources().openRawResource(background_id);
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(is, null, options);
+
+            imageHeight = options.outHeight;
+            imageWidth = options.outWidth;
+
+            zoom_level = ((float) imageHeight) / getHeight();  // we are always in 'fitY' mode
+            is.reset();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD_MR1) {
+                BitmapRegionDecoder brd = BitmapRegionDecoder.newInstance(is, true);
+
+                options.inJustDecodeBounds = false;
+                r.set(0, 0, Math.min((int) (getWidth() * ((max_num_pages + 4.0) / 5) * zoom_level), imageWidth), imageHeight);
+                saved_bitmap = brd.decodeRegion(r, options);
+                brd.recycle();
+            } else {
+                saved_bitmap = Bitmap.createBitmap(BitmapFactory.decodeStream(is), 0, 0, Math.min((int) (getWidth() * ((max_num_pages + 4.0) / 5) * zoom_level), imageWidth), imageHeight);
+            }
+
+            is.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Cannot decode: " + e.getMessage());
+            background_id = -1;
+            return;
+        }
+
+        saved_height = getHeight();
+        saved_width = getWidth();
+        background_saved_id = background_id;
+        saved_max_num_pages = max_num_pages;
+    }
+
+    int current_position;
+    float current_offset;
+
+    @Override
+    protected void onPageScrolled(int position, float offset, int offsetPixels) {
+        super.onPageScrolled(position, offset, offsetPixels);
+        current_position = position;
+        current_offset = offset;
+    }
+
+    int saved_position = -1;
+    float saved_offset;
+
+    Rect src = new Rect(), dst = new Rect();
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        src.set((int) (((current_position + current_offset) * getWidth() * zoom_level) / 5 ), 0,
+                (int) ((((current_position + current_offset) * getWidth() * zoom_level) / 5)  + (getWidth() * zoom_level)), imageHeight);
+
+        dst.set((int) ((current_position + current_offset) * getWidth()), 0, (int) ((current_position + current_offset) * getWidth()) + canvas.getWidth(), canvas.getHeight());
+        // still confused why we need to shift also in the destination canvas
+
+        canvas.drawBitmap(saved_bitmap, src, dst, null);
+
+        saved_position = current_position;
+        saved_offset = current_offset;
+    }
+
+    public void set_max_pages(int num_max_pages) {
+        max_num_pages = num_max_pages;
+        set_new_background();
+    }
+
+    public void setBackgroundAsset(int res_id) {
+        background_id = res_id;
+        set_new_background();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        set_new_background();
+    }
+}
